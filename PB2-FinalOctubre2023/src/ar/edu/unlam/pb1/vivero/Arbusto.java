323fb2771543ffae8fd60f3cc4071dffb7e6caf7
NicoGu <92960428+NickoGu@users.noreply.github.com>
2023-10-25 12:17:02 -0300

    Create

---
+++ PB2-FinalOctubre2023/src/ar/edu/unlam/pb1/vivero/Arbusto.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.pb1.vivero;
+
+public class Arbusto  {
+	
+	private final double GANANCIA_ARBUSTO = 1.6;
+	 
+	public Arbusto(int codigo, String nombre, double precio, int stock) {
+	}
+
+}
---
+++ PB2-FinalOctubre2023/src/ar/edu/unlam/pb1/vivero/Hierba.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.pb1.vivero;
+
+public class Hierba {
+
+	private final double gananciaHierbaMata = 1.2;
+
+	public Hierba(int codigo, String nombre, double precio, int stock) {
+	}
+}
